b09c2eae377ebaa3d2651fc7d8165b59341a478d
ely182 <ely.18gabriel@gmail.com>
2022-11-22 14:43:14 -0300

    atualizacao aula

--- src/templates/Home/Home.spec.jsx
+++ src/templates/Home/Home.spec.jsx
@@ -1,39 +1,39 @@
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
+
 import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
 import { Home } from '.';
+import userEvent from '@testing-library/user-event';
 
 const handlers = [
-  rest.get('*https://jsonplaceholder.typicode.com/posts', async (req, res, ctx) => {
-
-    return res([
-      ctx.json(
+  rest.get('*jsonplaceholder.typicode.com*', async (req, res, ctx) => {
+    return res(
+      ctx.json([
         {
           userId: 1,
           id: 1,
-          title: 'title 1',
-          body: 'body 1',
-          url:'img/img1'
+          title: 'title1',
+          body: 'body1',
+          url: 'img1.jpg',
         },
         {
           userId: 2,
           id: 2,
-          title: 'title 2',
-          body: 'body 2',
-          url:'img/img2'
+          title: 'title2',
+          body: 'body2',
+          url: 'img1.jpg',
         },
         {
           userId: 3,
           id: 3,
-          title: 'title 3',
-          body: 'body 3',
-          url:'img/img3'
-      ),
-    ]);
+          title: 'title3',
+          body: 'body3',
+          url: 'img3.jpg',
+        },
+      ]),
+    );
   }),
 ];
-
-
 
 const server = setupServer(...handlers);
 
@@ -48,12 +48,64 @@ describe('<Home />', () => {
     server.close();
   });
 
-  it('should render search,posts and load more', async () => {
-    const { debug } = render(<Home />);
-    const nMorePosts = screen.getByText('não existe postes =(');
+  it('should render search, posts and load more', async () => {
+    render(<Home />);
+    const noMorePosts = screen.getByText('Não existem posts =(');
 
-    await waitForElementToBeRemoved(nMorePosts);
-    screen.debug();
+    expect.assertions(3);
 
+    await waitForElementToBeRemoved(noMorePosts);
+
+    const search = screen.getByPlaceholderText(/type your search/i);
+    expect(search).toBeInTheDocument();
+
+    const images = screen.getAllByRole('img', { name: /title/i });
+    expect(images).toHaveLength(2);
+
+    const button = screen.getByRole('button', { name: /load more posts/i });
+    expect(button).toBeInTheDocument();
+  });
+
+  it('should search for posts', async () => {
+    render(<Home />);
+    const noMorePosts = screen.getByText('Não existem posts =(');
+
+    expect.assertions(10);
+
+    await waitForElementToBeRemoved(noMorePosts);
+
+    const search = screen.getByPlaceholderText(/type your search/i);
+
+    expect(screen.getByRole('heading', { name: 'title1 1' })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'title2 2' })).toBeInTheDocument();
+    expect(screen.queryByRole('heading', { name: 'title3 3' })).not.toBeInTheDocument();
+
+    userEvent.type(search, 'title1');
+    expect(screen.getByRole('heading', { name: 'title1 1' })).toBeInTheDocument();
+    expect(screen.queryByRole('heading', { name: 'title2 2' })).not.toBeInTheDocument();
+    expect(screen.queryByRole('heading', { name: 'title3 3' })).not.toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'Search value: title1' })).toBeInTheDocument();
+
+    userEvent.clear(search);
+    expect(screen.getByRole('heading', { name: 'title1 1' })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'title2 2' })).toBeInTheDocument();
+
+    userEvent.type(search, 'post does not exist');
+    expect(screen.getByText('Não existem posts =(')).toBeInTheDocument();
+  });
+
+  it('should load more posts', async () => {
+    render(<Home />);
+    const noMorePosts = screen.getByText('Não existem posts =(');
+
+    expect.assertions(2);
+
+    await waitForElementToBeRemoved(noMorePosts);
+
+    const button = screen.getByRole('button', { name: /load more posts/i });
+
+    userEvent.click(button);
+    expect(screen.getByRole('heading', { name: 'title3 3' })).toBeInTheDocument();
+    expect(button).toBeDisabled();
   });
 });
